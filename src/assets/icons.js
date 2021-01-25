a0fd70392fe6b474ec748aaf42fd5246a86d6873
Kamil Golec <kumalgfilms@gmail.com>
2021-01-25 19:14:55 +0100

    icon svgs

---
+++ src/assets/icons.js
@@ -0,0 +1,6 @@
+import { angleLeft } from './icons/angleLeft'
+import { angleRight } from './icons/angleRight'
+import { star } from './icons/star'
+import { starFill } from './icons/starFill'
+
+export { angleLeft, angleRight, star, starFill }
