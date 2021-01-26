1b543d5eed4bf43b0016d1f13e004f5b5ed74737
Kamil Golec <kumalgfilms@gmail.com>
2021-01-26 19:06:02 +0100

    selection in datepicker

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -6,20 +6,24 @@
   <button @click="nextMonth()">
     <Icon icon="angleRight"></Icon>
   </button>
+
+  <button @click="nextMonth()">
+    dateFrom
+  </button>
+  <button @click="nextMonth()">
+    dateFrom
+  </button>
+
   <div class="date-picker__calendar">
-    <div v-for="prevMonthDay in prevMonthDays" :key="`prev-${prevMonthDay}`" class="date-picker__calendar__pre-item">
-      {{ prevMonthDay }}
-    </div>
     <div
-      @click="selectDate(selectedYear, selectedMonth, day)"
-      v-for="day in daysInSelectedMonth"
-      :key="`current-${day}`"
-      class="date-picker__calendar__item"
+      @click="selectDate(dateObj.date)"
+      v-for="dateObj in allVisibleDays"
+      :key="dateObj.date.toString()"
+      :class="calendarItemClassList(dateObj)"
     >
-      {{ day }}
-    </div>
-    <div v-for="nextMonthDay in nextMonthDays" :key="`next-${nextMonthDay}`" class="date-picker__calendar__pre-item">
-      {{ nextMonthDay }}
+      <div class="date-picker__calendar__item__inner">
+        {{ dateObj.date.day }}
+      </div>
     </div>
   </div>
 </template>
@@ -45,6 +49,20 @@ export default {
     }
   },
   methods: {
+    calendarItemClassList({ otherMonth, selected, selectedFrom }) {
+      return [
+        'date-picker__calendar__item',
+        {
+          'date-picker__calendar__item--other-month': otherMonth
+        },
+        {
+          'date-picker__calendar__item--selected': selected
+        },
+        {
+          'date-picker__calendar__item--selected-from': selectedFrom
+        }
+      ]
+    },
     setDateFrom(val) {
       this.tempDateFrom = new DatePickerDate(2021, val, 25)
     },
@@ -67,8 +85,8 @@ export default {
         this.selectedMonth++
       }
     },
-    selectDate(year, month, day) {
-      this.tempDateFrom = new DatePickerDate(year, month, day)
+    selectDate(date) {
+      this.tempDateFrom = this.dateFrom !== null && this.dateFrom.equals(date) ? null : date
     },
     daysInMonth(year, month) {
       return new Date(year, month, 0).getDate()
@@ -80,14 +98,33 @@ export default {
       const prevMonthDate = new Date(currentMonthDate.setMonth(currentMonthDate.getMonth() - 1))
       const needsDays = Math.max(prevMonthDate.getDay() - 1, 0)
       const prevMonthDays = this.daysInMonth(prevMonthDate.getYear(), prevMonthDate.getMonth() + 1)
-      let array = Array.from({ length: prevMonthDays }, (_, i) => i + 1)
-      return array.slice(Math.max(array.length - needsDays, 0))
+      const array = Array.from({ length: prevMonthDays }, (_, i) => i + 1)
+      return array
+        .slice(Math.max(array.length - needsDays, 0))
+        .map(day => new DatePickerDate(prevMonthDate.getYear(), prevMonthDate.getMonth() + 1, day))
     },
     daysInSelectedMonth() {
-      return this.daysInMonth(this.selectedYear, this.selectedMonth)
+      const days = this.daysInMonth(this.selectedYear, this.selectedMonth)
+      return Array.from({ length: days }, (_, i) => i + 1).map(
+        day => new DatePickerDate(this.selectedYear, this.selectedMonth, day)
+      )
     },
     nextMonthDays() {
-      return 7 * 6 - this.prevMonthDays.length - this.daysInSelectedMonth
+      const days = 7 * 6 - this.prevMonthDays.length - this.daysInSelectedMonth.length
+      return Array.from({ length: days }, (_, i) => i + 1).map(
+        day => new DatePickerDate(this.selectedYear, this.selectedMonth + 1, day)
+      )
+    },
+    allVisibleDays() {
+      const allDays = [...this.prevMonthDays, ...this.daysInSelectedMonth, ...this.nextMonthDays]
+      return allDays.map(d => {
+        return {
+          date: d,
+          selected: this.dateFrom !== null && this.dateFrom.lessThan(d),
+          selectedFrom: this.dateFrom !== null && this.dateFrom.equals(d),
+          otherMonth: this.selectedYear !== d.year || this.selectedMonth !== d.month
+        }
+      })
     }
   },
   watch: {
@@ -107,16 +144,69 @@ export default {
     display: grid;
     grid-template-columns: repeat(7, 1fr);
 
-    &__pre-item,
     &__item {
       text-align: center;
       cursor: pointer;
       font-size: 0.875rem;
-      padding: 12px;
-    }
+      margin: 4px 0;
+      box-sizing: border-box;
+      position: relative;
 
-    &__pre-item {
-      color: $border;
+      &__inner {
+        position: relative;
+        $size: 40px;
+        height: $size;
+        width: $size;
+        margin: auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: $size;
+        box-sizing: border-box;
+        transition-property: background-color, color;
+        transition-duration: 0.2s;
+        transition-timing-function: ease;
+      }
+
+      &:not(&--selected-from):hover &__inner {
+        background-color: rgba($primary, 0.1);
+        color: $primary;
+      }
+
+      &--other-month &__inner {
+        color: $border;
+      }
+
+      &--selected &__inner {
+        color: $primary;
+      }
+
+      &--other-month#{ & }--selected &__inner {
+        color: rgba($primary, 0.25);
+      }
+
+      &--selected-from &__inner {
+        font-weight: bold;
+        color: #fff;
+        background-color: $primary;
+      }
+
+      &--selected,
+      &--selected-from {
+        &:before {
+          content: '';
+          position: absolute;
+          background: rgba($primary, 0.1);
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+        }
+      }
+
+      &--selected-from:before {
+        left: 50%;
+      }
     }
   }
 }
--- src/filters/DatePickerDate.js
+++ src/filters/DatePickerDate.js
@@ -7,4 +7,22 @@ export default class DatePickerDate {
   toString() {
     return `${this.year}-${this.month}-${this.day}`
   }
+  equals(date) {
+    if (date === null) return false
+    return this.year == date.year && this.month == date.month && this.day == date.day
+  }
+  lessThan(date) {
+    if (date === null) return false
+    if (this.year < date.year) return true
+    if (this.year === date.year && this.month < date.month) return true
+    if (this.year === date.year && this.month === date.month && this.day < date.day) return true
+    return false
+  }
+  greaterThan(date) {
+    if (date === null) return false
+    if (this.year > date.year) return true
+    if (this.year === date.year && this.month > date.month) return true
+    if (this.year === date.year && this.month === date.month && this.day > date.day) return true
+    return false
+  }
 }
