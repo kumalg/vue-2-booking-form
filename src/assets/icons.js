57e27a0367b00e81b92e7f8bfe074a5f0bc29d06
Kamil Golec <kumalgfilms@gmail.com>
2021-01-27 17:58:05 +0100

    additional icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,6 +1,9 @@
 import { angleLeft } from './icons/angleLeft'
 import { angleRight } from './icons/angleRight'
+import { arrowRight } from './icons/arrowRight'
+import { cross } from './icons/cross'
+import { crossSmall } from './icons/crossSmall'
 import { star } from './icons/star'
 import { starFill } from './icons/starFill'
 
-export { angleLeft, angleRight, star, starFill }
+export { angleLeft, angleRight, arrowRight, cross, crossSmall, star, starFill }
